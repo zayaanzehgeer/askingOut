2cfb3e82b9306e8bf6dc278d5d4b0e6e0c4720f8
Mohammad Zayaan Zehgeer <79523896+zayaanzehgeer@users.noreply.github.com>
2023-11-26 15:08:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 27th may!";
+  question.innerHTML = "Yay, on my way to see you!";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
